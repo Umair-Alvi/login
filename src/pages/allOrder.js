285e960745a09d871f64fdca4419d39effe8ce4a
Muhammad Umair Akram <165748503+Umair-Alvi@users.noreply.github.com>
2024-05-02 10:00:54 +0500

    Update allOrder.js
    oder page open when login

--- src/pages/allOrder.js
+++ src/pages/allOrder.js
@@ -1,6 +1,15 @@
+import getAuth from '@/getAuth';
+import store from '@/store';
+import { Router, useRouter } from 'next/router';
 import React from 'react'
 
 const allOrder = () => {
+    const state = store.getState(); // Get the current state from the Redux store
+    const isLoggedIn = getAuth(state);
+    const router = useRouter();
+    if (!isLoggedIn) {
+        router.push("/login");
+    }
   return (
     <div>allOrder page </div>
   )
